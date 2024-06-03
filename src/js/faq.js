b69eacaa943980bc312d15857c861a3ea49e69f4
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-06-03 11:45:14 +0300

    fix program

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,19 +1,19 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-$('.question').click(function () {
-  $(this).find('.content').toggle(200); //скрытие, показ ответа
-  if ($(this).hasClass('open')) {
-    $(this).removeClass('open');
+$(".question").click(function () {
+  $(this).find(".content").toggle(200); //скрытие, показ ответа
+  if ($(this).hasClass("open")) {
+    $(this).removeClass("open");
   } else {
-    $(this).addClass('open');
+    $(this).addClass("open");
   }
 });
 
-$('.question-two').click(function () {
-  $(this).find('.content-two').toggle(200); //скрытие, показ ответа
-  if ($(this).hasClass('open')) {
-    $(this).removeClass('open');
+$(".question-two").click(function () {
+  $(this).find(".content-two").toggle(200); //скрытие, показ ответа
+  if ($(this).hasClass("open")) {
+    $(this).removeClass("open");
   } else {
-    $(this).addClass('open');
+    $(this).addClass("open");
   }
 });
