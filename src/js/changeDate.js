449f3077b5758b304bad378fc54cf34da7a7321a
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-07-07 17:57:44 +0300

    update date

--- src/js/changeDate.js
+++ src/js/changeDate.js
@@ -1,7 +1,7 @@
 // change day
 
 var currentDate = new Date();
-currentDate.setDate(currentDate.getDate() + 1);
+currentDate.setDate(currentDate.getDate());
 var day = currentDate.getDate();
 var month = currentDate.getMonth() + 1;
 var year = currentDate.getFullYear();
